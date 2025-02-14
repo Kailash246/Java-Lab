f228240831a7ac68f011dbcc86d262bd7a97203a
Kailash246 <151384330+Kailash246@users.noreply.github.com>
2025-02-14 14:11:48 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,85 @@
-////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.util.*;
 //public class Main {
 //    public static void main(String[] args) {
-//        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-//        // to see how IntelliJ IDEA suggests fixing it.
-//        System.out.printf("Hello and welcome!");
+//        Scanner x=new Scanner(System.in);
+//        System.out.println("Enter First Name");
+//        String first=x.next();
+//        System.out.println("Enter Last Name");
+//        String last=x.next();
+//        System.out.println("Enter Id");
+//        int a=x.nextInt();
+//        System.out.println("Enter Mark 1");
+//        int b=x.nextInt();
+//        System.out.println("Enter Mark 2");
+//        int c=x.nextInt();
+//        int average = (b+c)/2;
+//        System.out.println("Student Name is " + first + last );
+//        System.out.println("Student Id is "+ a);
+//        System.out.println("The Average is "+ average);
+
+//        Scanner x=new Scanner(System.in);
+//        System.out.println("Enter Principal Amount");
+//        int p=x.nextInt();
+//        System.out.println("Enter the No. years");
+//        int t=x.nextInt();
+//        System.out.println("Enter the Rate of Interest");
+//        int r=x.nextInt();
+//        int amount = (p*t*r)/100;
+//        System.out.println("Total Interest is: "+ amount);
+//        System.out.println("Total Amount: " +(p+amount) );
+
+//        Scanner x=new Scanner(System.in);
+//        System.out.println("Enter Length");
+//        int l=x.nextInt();
+//        System.out.println(" Enter Breadth");
+//        int b=x.nextInt();
+//        int a=l*b;
+//        int p=2*(l+b);
+//        System.out.println("The Area is: " + (a+"sqm"));
+//        System.out.println("The Parameter is: "+ (p+"sqm"));
+
+//class Animal{
+//    public static void barking(){
+//        System.out.println("Dog is barking");
 //
-//        for (int i = 1; i <= 5; i++) {
-//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//            System.out.println("i = " + i);
-//        }
 //    }
 //}
+//class Behaviour extends Animal {
+//    public static void eating() {
+//        System.out.println("Dog is eating");
+//
+//
+//    }
+//}
+//public class Main12 {
+//    public static void main(String[]args){
+//        Behaviour obj1= new Behaviour();
+//
+//        obj.parent1();
+//        obj.Child1();
+//    }
+//}
+
+//
+//class Vehicles{
+//    public  static void car_company(){
+//        System.out.println("Car Company is : Benz");
+//    }
+//}
+//class Car extends Vehicles{
+//    public  static void car_price(){
+//        System.out.println("Car Price is : 50Lakhs");
+//    }
+//}
+//public class Main  {
+//    public  static void main(String[]args){
+//        System.out.println("Car Details");
+//        System.out.println("==========================");
+//        Car obj1 = new Car();
+//        obj1.car_company();
+//        obj1.car_price();
+//    }
+//}
+
+
