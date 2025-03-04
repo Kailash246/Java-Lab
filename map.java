550798ded664eb8367d5e9b59efa94cd1789c13f
Kailash246 <151384330+Kailash246@users.noreply.github.com>
2025-03-04 11:08:57 +0530

    Add files via upload

---
+++ map.java
@@ -0,0 +1,25 @@
+//COLLECTION MAPS IN JAVA
+import java.util.Map;
+import java.util.HashMap;
+
+class map {
+    public static void main(String[]args){
+        Map<String,Integer> country = new HashMap<>();//Creating Empty Map
+        System.out.println("COUMTRIES DETAIL");
+        System.out.println("");
+
+        country.put("INDIA",1);
+        country.put("CHINA",2);
+        country.put("NORTH KOREA",7);
+
+        System.out.println("COUMTRIES"+country);
+
+        for(Map.Entry<String,Integer>entry : country.entrySet()){
+            System.out.println("COUNTRIES :"+entry.getKey()+entry.getValue());
+        }
+
+
+
+
+    }
+}
